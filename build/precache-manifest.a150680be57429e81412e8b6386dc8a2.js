d8f1c7131150a0bfda2112fb48ca404ccd4bbbb5
manish <manishraghav9733@gmail.com>
2020-11-19 10:29:34 +0530

    assignment deployed

---
+++ build/precache-manifest.a150680be57429e81412e8b6386dc8a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca03d3159cfde008011168ca5a820a76",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "bccb497eae6b6b36c79b",
+    "url": "/assignment/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "58b0e56f43239ce441bc",
+    "url": "/assignment/static/css/main.57f876af.chunk.css"
+  },
+  {
+    "revision": "bccb497eae6b6b36c79b",
+    "url": "/assignment/static/js/2.dd07de0b.chunk.js"
+  },
+  {
+    "revision": "58b0e56f43239ce441bc",
+    "url": "/assignment/static/js/main.e11f519b.chunk.js"
+  },
+  {
+    "revision": "22ca2153820494013b7f",
+    "url": "/assignment/static/js/runtime~main.d5421ce1.js"
+  }
+]);
